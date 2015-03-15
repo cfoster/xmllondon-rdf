9ce967baaebdd368c56fe83cda8e080fcc6fd3d9
Charles Foster <charles@cfoster.net>
2015-03-15 13:50:46 +0000

    further reorganisation.

---
+++ doi_batch_xml/process.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+transform -s:doi_batch_xml_london_2013.xml -o:../2013-specifics.rdf -xsl:doi_batch2rdf.xsl
+transform -s:doi_batch_xml_london_2014.xml -o:../2014-specifics.rdf -xsl:doi_batch2rdf.xsl
